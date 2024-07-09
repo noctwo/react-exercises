9f55f38a7206b5423c56f5cbf010041f317900c2
noctwo <mr.evok@gmail.com>
2024-07-09 15:30:23 +0200

    updated cards section to using props

--- React-Project-One/src/assets/components/Cards/Cards.tsx
+++ React-Project-One/src/assets/components/Cards/Cards.tsx
@@ -1,37 +1,23 @@
 import "./Cards.css"
 
-const Cards = () => {
-    return (
-        <section className="cards-wrapper">
-            <div className ="card">
-                <div className="card-img">
-                <img src="/card-01.jpg"></img>
-                </div>
-                <div className="card-text">
-                    <h3>Lorem ipsum dolor sit amet.</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo dolorum, sequi doloremque temporibus minus cupiditate?</p>
-                </div>
-            </div>
-            <div className ="card">
-                <div className="card-img">
-                <img src="/card-02.jpg"></img>
-                </div>
-                <div className="card-text">
-                    <h3>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, officia?</p>
-                </div>
-            </div>
-            <div className ="card">
-                <div className="card-img">
-                <img src="/card-03.jpg"></img>
-                </div>
-                <div className="card-text">
-                    <h3>Lorem ipsum dolor</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, accusantium ut! Quos assumenda praesentium sapiente deleniti cumque ratione odio numquam.</p>
-                </div>
-            </div>
-        </section>
-    );
+type CardContent = {
+    title: string,
+    image: string,
+    text: string
 }
 
+const Cards: React.FC<CardContent> = (props) => {
+
+    return(
+        <div className ="card">
+                <div className="card-img">
+                <img src={props.image}></img>
+                </div>
+                <div className="card-text">
+                    <h3>{props.title}</h3>
+                    <p>{props.text}</p>
+                </div>
+            </div>
+    )
+}
 export default Cards;
--- React-Project-One/src/assets/pages/Home.tsx
+++ React-Project-One/src/assets/pages/Home.tsx
@@ -10,7 +10,11 @@ const Home = () => {
         <Header />
         <Hero />
         <Intro />
-        <Cards />
+        <section className="cards-wrapper">
+        <Cards image="/card-01.jpg" title="Lorem ipsum dolor sit amet." text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo dolorum, sequi doloremque temporibus minus cupiditate?" />
+        <Cards image="/card-02.jpg" title="Lorem ipsum dolor sit amet consectetur adipisicing." text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, officia?" />
+        <Cards image="/card-03.jpg" title="Lorem ipsum dolor" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, accusantium ut! Quos assumenda praesentium sapiente deleniti cumque ratione odio numquam." />
+        </section>
         <Footer />
         </>
     );
